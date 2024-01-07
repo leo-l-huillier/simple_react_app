c0fdfcd4aec3dc5473d070ce36ded4973b0e462c
Leo LHUILLIER <leo.l-huillier@epitech.eu>
2024-01-07 18:50:29 +0100

    feat: clearer app

--- simpleReactApp/src/pages/SecondPage.tsx
+++ simpleReactApp/src/pages/SecondPage.tsx
@@ -1,11 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+    Center,
+    Text,
+} from '@chakra-ui/react';
 
 interface EventData {
     date_start: string;
     title: string;
     price_detail: string;
+    formattedDate?: string;
+    formattedPrice?: string;
 }
 
 const SecondPage: React.FC = () => {
@@ -21,8 +35,9 @@ const SecondPage: React.FC = () => {
     useEffect(() => {
         axios({
             method: 'get',
-            url: `https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/que-faire-a-paris-/records?limit=20`,
+            url: `https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/que-faire-a-paris-/records?limit=100`,
         }).then(res => {
+            console.log('res = ', res)
             setListAction(res.data.results)
         }).catch(error => {
             console.log('error = ', error)
@@ -31,42 +46,142 @@ const SecondPage: React.FC = () => {
 
     useEffect(() => {
         if (startDate !== null && endDate !== null) {
-            for (let i = 0; i < listAction.length; i++) {
-                console.log('listAction[i] = ', listAction[i].date_start)
-                console.log('listAction[i] = ', listAction[i].title)
-                console.log('listAction[i] = ', listAction[i].price_detail)
-            }
-
-            setFilteredListAction(listAction.filter((event: EventData) => {
-                const eventDate = new Date(event.date_start);
-                return eventDate >= startDate && eventDate <= endDate;
-            }));
+            setFilteredListAction(
+                listAction
+                    .filter((event: EventData) => {
+                        const eventDate = new Date(event.date_start);
+                        return eventDate >= startDate && eventDate <= endDate;
+                    })
+                    .map((event: EventData) => ({
+                        ...event,
+                        formattedDate: new Date(event.date_start).toLocaleString(),
+                        // formattedPrice: event.price_detail.replace(/<\/?[^>]+(>|$)/g, ''),
+                        formattedPrice: event.price_detail,
+                    }))
+            );
+            // console.log('filteredListAction = ', filteredListAction)
         }
     }, [listAction]);
 
+    function Title() {
+        return (
+            <Center>
+                <Text fontSize="3xl">Liste des evenements Sur Paris</Text>
+            </Center>
+        );
+    }
+
+    function DisplayTab() {
+        return (
+            <Center>
+                <TableContainer w="75%">
+                    <Table variant="simple">
+                        <TableCaption>Que faire a paris</TableCaption>
+                        <Thead>
+                            <Tr>
+                                <Th>Evenement</Th>
+                                <Th>Date de début</Th>
+                                <Th>Prix</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {filteredListAction.map((event: EventData, index: number) => (
+                                <Tr key={index}>
+                                    <Td>{event.title}</Td>
+                                    <Td>{event.formattedDate}</Td>
+                                    <Td>{event.formattedPrice}</Td>
+                                </Tr>
+                            ))}
+                        </Tbody>
+                    </Table>
+                </TableContainer>
+            </Center>
+        );
+    }
+
     return (
         <div>
-            <h2>Event List</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Date de début</th>
-                        <th>Titre de l'évènement</th>
-                        <th>Tarif</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredListAction.map((event: EventData, index: number) => (
-                        <tr key={index}>
-                            <td>{event.date_start}</td>
-                            <td>{event.title}</td>
-                            <td>{event.price_detail}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <Title />
+            <DisplayTab />
         </div>
     );
 };
 
 export default SecondPage;
+
+
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import { useLocation } from 'react-router-dom';
+
+// interface EventData {
+//     date_start: string;
+//     title: string;
+//     price_detail: string;
+// }
+
+// const SecondPage: React.FC = () => {
+//     const location = useLocation();
+//     const searchParams = new URLSearchParams(location.search);
+
+//     const startDate = new Date(searchParams.get('startDate') || '');
+//     const endDate = new Date(searchParams.get('endDate') || '');
+//     console.log('_____')
+//     console.log('startDate = ', startDate)
+//     console.log('endDate = ', endDate)
+
+//     const [listAction, setListAction] = useState<any>([])
+//     const [filteredListAction, setFilteredListAction] = useState<any>([])
+
+//     useEffect(() => {
+//         axios({
+//             method: 'get',
+//             url: `https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/que-faire-a-paris-/records?limit=20`,
+//         }).then(res => {
+//             setListAction(res.data.results)
+//         }).catch(error => {
+//             console.log('error = ', error)
+//         })
+//     }, []);
+
+//     useEffect(() => {
+//         if (startDate !== null && endDate !== null) {
+//             for (let i = 0; i < listAction.length; i++) {
+//                 console.log('listAction[i] = ', listAction[i].date_start)
+//                 console.log('listAction[i] = ', listAction[i].title)
+//                 console.log('listAction[i] = ', listAction[i].price_detail)
+//             }
+
+//             setFilteredListAction(listAction.filter((event: EventData) => {
+//                 const eventDate = new Date(event.date_start);
+//                 return eventDate >= startDate && eventDate <= endDate;
+//             }));
+//         }
+//     }, [listAction]);
+
+//     return (
+//         <div>
+//             <h2>Event List</h2>
+//             <table>
+//                 <thead>
+//                     <tr>
+//                         <th>Date de début</th>
+//                         <th>Titre de l'évènement</th>
+//                         <th>Tarif</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     {filteredListAction.map((event: EventData, index: number) => (
+//                         <tr key={index}>
+//                             <td>{event.date_start}</td>
+//                             <td>{event.title}</td>
+//                             <td>{event.price_detail}</td>
+//                         </tr>
+//                     ))}
+//                 </tbody>
+//             </table>
+//         </div>
+//     );
+// };
+
+// export default SecondPage;
--- simpleReactApp/src/pages/firstPage.tsx
+++ simpleReactApp/src/pages/firstPage.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect, useCallback, KeyboardEvent } from 'react';
 import '../app/App.css';
-import { } from '@chakra-ui/react';
+import { Input, Center, Text, Button, Link } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 
 const DateSelectionPage: React.FC = () => {
     const [startDate, setStartDate] = useState<Date | null>(null);
     const [endDate, setEndDate] = useState<Date | null>(null);
     const [isEndDateEnabled, setIsEndDateEnabled] = useState(false);
+    const [isEndDateSet, setIsEndDateSet] = useState(false);
 
     const navigate = useNavigate();
 
@@ -35,6 +36,15 @@ const DateSelectionPage: React.FC = () => {
     const handleEndDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const selectedEndDate = new Date(event.target.value);
         setEndDate(selectedEndDate);
+        setIsEndDateSet(true);
+    };
+
+    const changePage = () => {
+        console.log('startDate = ', startDate)
+        console.log('endDate = ', endDate)
+        if (startDate && endDate) {
+            navigate(`/secondPage?startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}`);
+        }
     };
 
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -43,25 +53,88 @@ const DateSelectionPage: React.FC = () => {
         }
     };
 
+    function Title() {
+        return (
+            <Center>
+                <Text mt={20} fontSize="3xl">Liste des evenements Sur Paris</Text>
+            </Center>
+        );
+    }
+
+    function SubTitle() {
+        return (
+            <Center>
+                <Text fontSize="xl">Pour quand cherchez vous un évenement ?</Text>
+            </Center>
+        );
+    }
+
+    function FirstDateInput() {
+        return (
+            <Center>
+                <Input
+                    mt={10}
+                    w="17%"
+                    type="date"
+                    value={startDate ? startDate.toISOString().split('T')[0] : ''}
+                    onChange={handleStartDateChange}
+                />
+            </Center>
+        );
+    }
+
+    function SecondDateInput() {
+        return (
+            <Center>
+                <Input
+                    mt={10}
+                    w="17%"
+                    type="date"
+                    value={endDate ? endDate.toISOString().split('T')[0] : ''}
+                    min={startDate?.toISOString().split('T')[0]}
+                    max={calculateEndDateLimit()?.toISOString().split('T')[0]}
+                    onChange={handleEndDateChange}
+                />
+            </Center>
+        );
+    }
+
+    function DisplayButton() {
+        return (
+            <Center>
+                <Button onClick={() => changePage()} mt={10} colorScheme='telegram'>
+                    {/* <Link color='white' href='/secondPage'> */}
+                    Chercher des évenements
+                    {/* </Link> */}
+                </Button>
+            </Center>
+        );
+    }
+
     return (
-        <div className="container" tabIndex={0} onKeyDown={handleKeyDown}>
-            <label>Date de début:</label>
-            <input
-                type="date"
-                value={startDate ? startDate.toISOString().split('T')[0] : ''}
-                onChange={handleStartDateChange}
-            />
+        <div className="container" tabIndex={0} >
+            <Title />
+            <SubTitle />
+            {/* <Center>
+                <input
+                    // mt={10}
+                    // w="17%"
+                    type="date"
+                    value={startDate ? startDate.toISOString().split('T')[0] : ''}
+                    onChange={handleStartDateChange}
+                />
+            </Center> */}
+            <FirstDateInput />
 
             {isEndDateEnabled && (
                 <div>
-                    <label>Date de fin:</label>
-                    <input
-                        type="date"
-                        value={endDate ? endDate.toISOString().split('T')[0] : ''}
-                        min={startDate?.toISOString().split('T')[0]}
-                        max={calculateEndDateLimit()?.toISOString().split('T')[0]}
-                        onChange={handleEndDateChange}
-                    />
+                    <SecondDateInput />
+                </div>
+            )}
+
+            {endDate !== null && (
+                <div>
+                    <DisplayButton />
                 </div>
             )}
         </div>
@@ -74,4 +147,124 @@ export const FirstPage = (): JSX.Element => (
         <DateSelectionPage />
     </div>
 );
+
+// import React, { useState, useEffect, useCallback, KeyboardEvent } from 'react';
+// import '../app/App.css';
+// import { } from '@chakra-ui/react';
+// import { useNavigate } from 'react-router-dom';
+
+// const DateSelectionPage: React.FC = () => {
+//     const [startDate, setStartDate] = useState<Date | null>(null);
+//     const [endDate, setEndDate] = useState<Date | null>(null);
+//     const [isEndDateEnabled, setIsEndDateEnabled] = useState(false);
+
+//     const navigate = useNavigate();
+
+//     const calculateEndDateLimit = useCallback(() => {
+//         if (startDate) {
+//             const endDateLimit = new Date(startDate);
+//             endDateLimit.setDate(endDateLimit.getDate() + 3);
+//             return endDateLimit;
+//         }
+//         return null;
+//     }, [startDate]);
+
+//     useEffect(() => {
+//         const endDateLimit = calculateEndDateLimit();
+//         if (endDate && endDateLimit && endDate > endDateLimit) {
+//             setEndDate(null);
+//         }
+//     }, [startDate, endDate, calculateEndDateLimit]);
+
+//     const handleStartDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//         const selectedStartDate = new Date(event.target.value);
+//         setStartDate(selectedStartDate);
+//         setIsEndDateEnabled(true);
+//     };
+
+//     const handleEndDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//         const selectedEndDate = new Date(event.target.value);
+//         setEndDate(selectedEndDate);
+//     };
+
+//     const handleKeyDown = (event: KeyboardEvent) => {
+//         // if (event.key === 'Enter' && startDate && endDate) {
+//         //     navigate(`/secondPage?startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}`);
+//         // }
+//     };
+
+//     const changePage = () => {
+//         console.log('startDate = ', startDate)
+//         console.log('endDate = ', endDate)
+//         // if (startDate && endDate) {
+//         //     navigate(`/secondPage?startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}`);
+//         // }
+//     };
+
+//     function Title() {
+//         return (
+//             <Center>
+//                 <Text mt={20} fontSize="3xl">Liste des evenements Sur Paris</Text>
+//             </Center>
+//         );
+//     }
+
+//     function SubTitle() {
+//         return (
+//             <Center>
+//                 <Text fontSize="xl">Pour quand cherchez vous un évenement ?</Text>
+//             </Center>
+//         );
+//     }
+
+//     function DisplayButton() {
+//         return (
+//             <Center>
+//                 <Button onClick={() => changePage} mt={10} colorScheme='telegram'>
+//                     <Link color='white' href='/secondPage'>
+//                         Chercher des évenements
+//                     </Link>
+//                 </Button>
+//             </Center>
+//         );
+//     }
+
+//     return (
+//         <div className="container" tabIndex={0} onKeyDown={handleKeyDown}>
+//             <Title />
+//             <SubTitle />
+//             <Input
+//                 type="date"
+//                 value={startDate ? startDate.toISOString().split('T')[0] : ''}
+//                 onChange={handleStartDateChange}
+//             />
+
+//             {isEndDateEnabled && (
+//                 <div>
+//                     <label>Date de fin:</label>
+//                     <Input
+//                         type="date"
+//                         value={endDate ? endDate.toISOString().split('T')[0] : ''}
+//                         min={startDate?.toISOString().split('T')[0]}
+//                         max={calculateEndDateLimit()?.toISOString().split('T')[0]}
+//                         onChange={handleEndDateChange}
+//                     />
+//                 </div>
+//             )}
+
+//             {endDate !== null && (
+//                 <div>
+//                     <DisplayButton />
+//                 </div>
+//             )}
+//         </div>
+//     );
+// };
+
+
+// export const FirstPage = (): JSX.Element => (
+//     <div>
+//         <DateSelectionPage />
+//     </div>
+// );
 
